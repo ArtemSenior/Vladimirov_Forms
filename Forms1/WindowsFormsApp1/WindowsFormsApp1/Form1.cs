0f3cfbfbde3aff345c20d5a71eb4dbaa1d7be185
ArtemSenior <119601918+ArtemSenior@users.noreply.github.com>
2022-12-01 16:44:41 +0300

    Add files via upload

---
+++ Forms1/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double p, s;
+            try
+            {
+                p = Convert.ToDouble(textBox1.Text);
+                s = Math.Pow(p, 2) * Math.Sqrt(3) / 36;
+                textBox2.Text = "" + s;
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("Вводи только числа!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+            }
+        }
+    }
+}
---
+++ Forms1/WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = 0;
+            {
+                
+                    if (string.IsNullOrWhiteSpace(textBox1.Text))
+                    {
+                        MessageBox.Show("Поле не заполнено!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                a = Convert.ToInt32(textBox1.Text);
+                if (a % 2 == 0)
+                {
+                        textBox2.Text = " четным";
+                }
+                    else
+                    {
+                            textBox2.Text = " нечетным";
+                    
+                    }
+        }
+        }
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e) // вводить можно лишь числовые значения
+        {
+            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != 8)
+            {
+                e.Handled = true;
+            }
+
+        }
+    }
+}
