cc3463eda543ea06b8eaa2a472d28dd1526a87a9
ArtemSenior <119601918+ArtemSenior@users.noreply.github.com>
2022-12-02 22:44:20 +0300

    Add files via upload

---
+++ Console3/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Hello
+{
+    class Program
+    {
+        static double min(double a, double b)
+        {
+            return (a < b) ? a : b;
+        }
+
+        static void Main(string[] args)
+        {
+            double x, y, z, v;
+            do
+            {
+                try
+                {
+                    Console.Write("x=");
+                    x = double.Parse(Console.ReadLine());
+
+                    Console.Write("y=");
+                    y = double.Parse(Console.ReadLine());
+
+                    Console.Write("z=");
+                    z = double.Parse(Console.ReadLine());
+
+                    Console.Write("v=");
+                    v = double.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Вводите числа");
+                }
+            }
+            while (true);
+
+            double q = min(x, y);
+            double w = min(z, v);
+            double res = min(q, w);
+
+            Console.WriteLine(res);
+        }
+    }
+}
---
+++ Console3/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static double f(double x)
+        {
+            double y = 0;
+            if (Math.Abs(x) < (Math.PI / 2)) y = Math.Sin(x);
+            else if (Math.Abs(x) <= Math.PI) y = Math.Cos(x);
+            else y = 0;
+            return y;
+        }
+        static void Main(string[] args)
+        {
+            double a;
+            double b;
+            double h;
+            Console.Write("a=");
+            while (true)
+            {
+                if (!double.TryParse(Console.ReadLine(), out a))
+                {
+                    Console.Write("Ошибка! Введите число a=");
+                    continue;
+                }
+                else break;
+            }
+            Console.Write("b=");
+            while (true)
+            {
+                if (!double.TryParse(Console.ReadLine(), out b))
+                {
+                    Console.Write("Ошибка! Введите число b=");
+                    continue;
+                }
+                else break;
+            }
+            while (true)
+            {
+                if (a > b)
+                {
+                    Console.Write("a не может быть больше b");
+                    Console.Write("\na=");
+                    a = double.Parse(Console.ReadLine());
+                    Console.Write("b=");
+                    b = double.Parse(Console.ReadLine());
+                    continue;
+                }
+                else break;
+            }
+            Console.Write("h=");
+            while (true)
+            {
+                if (!double.TryParse(Console.ReadLine(), out h))
+                {
+                    Console.Write("Ошибка! Введите число h=");
+                    continue;
+                }
+                else if (h <= 0)
+                {
+                    Console.Write("Шаг не может быть отрицательным или равным 0! h=");
+                    continue;
+                }
+                else break;
+            }
+            for (double i = a; i <= b; i += h)
+                Console.WriteLine("f({0:f2})={1:f4}", i, f(i));
+        }
+    }
+}
