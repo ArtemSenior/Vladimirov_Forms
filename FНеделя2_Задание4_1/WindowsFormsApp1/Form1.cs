4628fdbd10d7f754e6e36a38f9d91c816dbd9a69
ArtemSenior <119601918+ArtemSenior@users.noreply.github.com>
2022-12-09 00:17:16 +0300

    Add files via upload

---
+++ FНеделя2_Задание4_1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static double F(double n)
+        {
+            if (n == 1) return -10;
+            if (n == 2) return 2;
+            return Math.Abs(F(n - 2)) - 6 * F(n - 1);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            do
+            {
+                try
+                {
+
+                    double x = Convert.ToDouble(textBox1.Text);
+                    if (x <= 0)
+                    {
+                        MessageBox.Show("Нельзя вводить числа меньше 0!");
+                        break;
+                    }
+                    else
+                    {
+                        richTextBox1.Text += F(x);
+                        break;
+                    }
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Вводить только числа");
+                    break;
+                }
+            }
+            while (true);
+
+        }
+    }
+}
---
+++ FНеделя2_Задание4_2/FНеделя2_Задание4_2/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FНеделя2_Задание4_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static void Rec(double a0, double d, int n, double s, out double an, out double sum)
+        {
+            an = a0;
+            s += a0;
+            sum = s;
+            if (n == 1) return;
+            Rec(a0 + d, d, n - 1, s, out an, out sum);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            do
+            {
+                double a0 = 0;
+                double d = 0;
+                int n = 0;
+                do
+                {
+                    try
+                    {
+                        a0 = double.Parse(textBox1.Text);
+                        break;
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Вводите только числа");
+                        break;
+                    }
+                }
+                while (true);
+
+                do
+                {
+                    try
+                    {
+                        d = double.Parse(textBox2.Text);
+                        break;
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Вводите только числа");
+                        break;
+                    }                    
+                }                
+                while (true);
+
+                do
+                {                   
+                    try
+                    {
+                        n = int.Parse(textBox3.Text);
+                        break;
+                    }
+                    catch (Exception)
+                    {
+                        MessageBox.Show ("Введите числа, где n > 1");
+                        break;
+                    }
+                }
+                while (true);
+
+                double an, sum;
+                Rec(a0, d, n, 0, out an, out sum);
+                richTextBox1.Text = ($"An = {an}, sum = {sum}");
+                break;
+
+            } while (true);
+        }
+    }
+}
+
+
