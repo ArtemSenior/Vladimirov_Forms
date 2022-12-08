188fefaa594d65f19a9407f193c3988266204fad
ArtemSenior <119601918+ArtemSenior@users.noreply.github.com>
2022-12-08 19:06:07 +0300

    Add files via upload

---
+++ FНеделя2_Задание5/FНеделя2_Задание5/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FНеделя2_Задание5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static double f(double x)
+        {
+            try
+            {
+                //если х не попадает в область определения, то генерируется исключение 
+                if (x == 1) throw new Exception();
+                else return 1 / (Math.Pow(x, 2) - 1);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            try
+            {
+
+                double a = double.Parse(textBox1.Text);
+                double b = double.Parse(textBox2.Text);
+                double h = double.Parse(textBox3.Text);
+                if (b > a && h >= 0)
+                {
+                    for (double i = a; i <= b; i += h)
+                        try
+                        {
+                            richTextBox1.Text += $"y(" + i + ")=" + f(i) + "\n";
+                        }
+                        catch
+                        {
+                            richTextBox1.Text += $"y(" + i + ") = Ошибка\n";
+                        }
+                }
+                else
+                {
+                    MessageBox.Show("b не должно превышать а, h должно быть больше 0");
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Неверный формат ввода данных");
+            }
+            catch
+            {
+                MessageBox.Show("Неизвестная ошибка");
+            }
+        }
+    }
+}
